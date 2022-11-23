b49f3a8e48ca8430a997de6f779af56531457fbd
maniprakash245 <118921902+maniprakash245@users.noreply.github.com>
2022-11-23 22:20:25 +0530

    Add files via upload

---
+++ addthreenum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int num1, num2, num3, sum;
+    
+  cout << "Enter three integers: ";
+  cin >> num1 >> num2 >> num3;
+
+  sum = num1 + num2 + num3;
+
+  cout << sum;     
+
+  return 0;
+}
